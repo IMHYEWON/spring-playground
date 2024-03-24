d50cd93439a7b9963affbcf50d972c9121f2d958
IMHYEWON <hyewon1118@gmail.com>
2024-03-24 23:22:52 +0900

    Feat : @Recover Fallback 메소드

--- src/main/java/com/hyewon/springplayground/retry/Controller.java
+++ src/main/java/com/hyewon/springplayground/retry/Controller.java
@@ -1,11 +1,14 @@
 package com.hyewon.springplayground.retry;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static java.lang.Thread.sleep;
 
+@Slf4j
 @RestController
 public class Controller {
 
@@ -15,7 +18,7 @@ public class Controller {
         return retryMethod();
     }
 
-    @Retryable(retryFor = IllegalArgumentException.class)
+    @Retryable(retryFor = IllegalArgumentException.class, maxAttempts = 2)
     @GetMapping("/retry2")
     public String outer2() {
         return retryMethod2();
@@ -31,5 +34,11 @@ public class Controller {
         System.out.println("retryMethod2");
 
         throw new IllegalArgumentException("retryMethod2");
+    }
+
+    @Recover
+    private String fallBack(Exception e) {
+        log.warn("fallBack  {}", e.getMessage());
+        return null;
     }
 }
